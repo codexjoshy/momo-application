b118d02b186b10f1c8af2c8a3907f28dffa6fec3
codexjoshy <ebhoriajosh@gmail.com>
2023-03-11 19:48:05 +0100

    addded script for symlink

--- symlink.php
+++ symlink.php
@@ -1,6 +1,7 @@
 <?php
 
-
+var_dump($_SERVER['DOCUMENT_ROOT']);
+exit;
 $targetFolder = $_SERVER['DOCUMENT_ROOT'] . '/../momo-application/storage/app/public';
 $linkFolder = $_SERVER['DOCUMENT_ROOT'] . '/winners/storage/';
 symlink($targetFolder, $linkFolder) or die("error creating symlink");
