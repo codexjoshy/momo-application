d8a200433a731a269738eea9e1df37435d81ddff
codexjoshy <ebhoriajosh@gmail.com>
2023-03-11 18:22:05 +0100

    changed app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 
     <!-- Scripts -->
     {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
-    {{-- @vite([ 'resources/js/app.js']) --}}
+    @vite([ 'resources/js/app.js'])
 </head>
 <body>
     <div id="app">
