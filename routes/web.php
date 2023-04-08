de27d9ef964608e270d5125fbd9d6aa0b86cc3b6
codexjoshy <ebhoriajosh@gmail.com>
2023-04-08 22:17:32 +0100

    added home route for customers

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [App\Http\Controllers\Auth\LoginController::class,'showLoginForm']);
-// Route::get('/home', function () {
-//     return view('home');
-// });
+Route::get('/home', function () {
+    $smsBalance = [];
+    return view('home', compact('smsBalance'));
+});
 
 
 Route::get('/test', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
