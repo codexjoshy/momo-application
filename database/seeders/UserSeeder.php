fabd3e2015f493c1612e7b2bb204ae88dfd901e4
codexjoshy <ebhoriajosh@gmail.com>
2023-04-08 22:21:39 +0100

    added admin test account

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
-use Database\Seeders\Traits\HandleForeignKeys;
-use Database\Seeders\Traits\TruncateTable;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Database\Seeders\Traits\TruncateTable;
+use Database\Seeders\Traits\HandleForeignKeys;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
@@ -22,9 +23,13 @@ class UserSeeder extends Seeder
         \App\Models\User::factory(2)->create();
         $this->enableForeignKey();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'admin@tetragrammatongroup.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'authority'=> 'admin',
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
