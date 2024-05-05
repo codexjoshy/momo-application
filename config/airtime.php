c56744a6e2feb6bc869ddb19aeae0b2bd4476482
codexjoshy <ebhoriajosh@gmail.com>
2024-05-05 20:56:29 +0100

    fixed home bug

--- config/airtime.php
+++ config/airtime.php
@@ -1,6 +1,6 @@
 <?php
 return [
-  'base_url' => env('AIRTIME_BASE_URL'),
+  'base_url' => env('AIRTIME_BASE_URL', 'http://64.226.97.232:27171/jmg/web.html'),
   'username' => env('AIRTIME_USERNAME'),
   'apiKey' => env('AIRTIME_API_KEY'),
   'smsFrom' => env('AIRTIME_SMS_FROM')
